f0fb68474edcdcf01e7732b71369344c4df56d8d
CelebrateYourself <regnainania@mail.ru>
2019-08-24 18:33:50 +0300

    cfg: update package setup settings, add LICENSE

--- setup.py
+++ setup.py
@@ -8,6 +8,10 @@ with open(join(dirname(__file__), 'README.md')) as readme:
 setup(
     name='hh',
     version='0.3.0',
-    packages=find_packages(),
+    author='CelebrateYourself',
+    description='CLI utility for use hh.ru API',
     long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CelebrateYourself/util-hh",
+    packages=find_packages(),
 )
